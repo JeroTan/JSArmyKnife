89bf0c788eff53a12cbb6fed42faa6d9942ef37e
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2025-02-19 10:05:29 +0800

    update icon package

--- src/native/icon/iconify.ts
+++ src/native/icon/iconify.ts
@@ -15,9 +15,6 @@ export interface ICON{
   svg: SVG;
   vectors: VECTORS[];
 }
-export interface ICONLIST{
-  [key: (string|number)]: ICON
-}
 
 //A class that you will be use to add those Icons
 export class MakeIcon{
@@ -88,7 +85,7 @@ export function iconNew(){
 }
 
 
-export const IconList:ICONLIST = {
+export const IconList = {
   person: iconNew().addFill("M12 12q-1.65 0-2.825-1.175T8 8q0-1.65 1.175-2.825T12 4q1.65 0 2.825 1.175T16 8q0 1.65-1.175 2.825T12 12Zm-8 8v-2.8q0-.85.438-1.563T5.6 14.55q1.55-.775 3.15-1.163T12 13q1.65 0 3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2V20H4Z").get(),
 
   close: iconNew().addFill("M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504L738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512L828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496L285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512L195.2 285.696a64 64 0 0 1 0-90.496z").addViewBox("0 0 1024 1024").get(),
