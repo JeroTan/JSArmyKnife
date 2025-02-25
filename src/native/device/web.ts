155bf36f373f46a24b57a3961958b739e84c0043
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2025-02-25 12:02:23 +0800

    added device helper

---
+++ src/native/device/web.ts
@@ -0,0 +1,9 @@
+export function getCurrentLocation(): Promise<{error:false|GeolocationPositionError, data:GeolocationPosition|null}>{
+  return new Promise((resolve, reject)=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      resolve({error:false, data:position});
+    }, (error)=>{
+      resolve({error:error, data:null});
+    });
+  });
+}
