1a641488b848e23b76a999ef57a214846bc22ae6
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2025-06-11 09:26:12 +0800

    update package.json

--- src/native/storage/state.ts
+++ src/native/storage/state.ts
@@ -5,22 +5,33 @@ export class State{
   private stateList:STATE_STACK_DATA<Subscription<any>> = {};
 
   public addState<DATA_TYPE>(stateKey:string|number, value?:DATA_TYPE, silent = false){
-      if(this.stateList[stateKey] == undefined)
-          return this.stateList[stateKey] = new Subscription<DATA_TYPE>(value, silent);
-      else{
-          return this.stateList[stateKey] as Subscription<DATA_TYPE>;
-      }
+	  if(this.stateList[stateKey] == undefined)
+		  return this.stateList[stateKey] = new Subscription<DATA_TYPE>(value, silent);
+	  else{
+		  return this.stateList[stateKey] as Subscription<DATA_TYPE>;
+	  }
   }
 
+	public newState<DATA_TYPE>(value?:DATA_TYPE, silent = false, stateKey?:string|number){
+		if(stateKey == undefined){
+			stateKey = this.stateList ? Object.keys(this.stateList).length : 0;
+		}
+		if(this.stateList[stateKey] == undefined)
+		  return this.stateList[stateKey] = new Subscription<DATA_TYPE>(value, silent);
+	  else{
+		  return this.stateList[stateKey] as Subscription<DATA_TYPE>;
+	  }
+	}
+
   public select<ACTUAL_TYPE>(stateKey:string|number, value?:ACTUAL_TYPE){
-      const subs = this.stateList[stateKey] as Subscription<ACTUAL_TYPE>;
-      if(subs == undefined){
-          this.stateList[stateKey] = new Subscription<ACTUAL_TYPE>(value, true);
-      }
-      if(value){
-          this.stateList[stateKey].update(value);
-      }
-      return this.stateList[stateKey] as Subscription<ACTUAL_TYPE>;
+	  const subs = this.stateList[stateKey] as Subscription<ACTUAL_TYPE>;
+	  if(subs == undefined){
+		  this.stateList[stateKey] = new Subscription<ACTUAL_TYPE>(value, true);
+	  }
+	  if(value){
+		  this.stateList[stateKey].update(value);
+	  }
+	  return this.stateList[stateKey] as Subscription<ACTUAL_TYPE>;
   }
 }
 
@@ -30,60 +41,60 @@ export class Subscription<DATA_TYPE>{
   private stateTrigger = document.createElement("button");
 
   constructor(value?:DATA_TYPE, silent = false){
-      if(value)
-          this.set(value, silent);
+	  if(value)
+		  this.set(value, silent);
   }
 
   //--Setter--//
   set(value:DATA_TYPE, silent = false){
-      if(this.oldValue === this.value){
-          this.oldValue = value;
-      }
-      this.value = value;
-      if(!silent){
-          this.stateTrigger.dispatchEvent(new Event("click"));
-      }
-      return this;
+	  if(this.oldValue === this.value){
+		  this.oldValue = value;
+	  }
+	  this.value = value;
+	  if(!silent){
+		  this.stateTrigger.dispatchEvent(new Event("click"));
+	  }
+	  return this;
   }
   //--Setter--//
 
   get(){
-      return this.value;
+	  return this.value;
   }
 
   trigger(){
-      this.stateTrigger.dispatchEvent(new Event("click"));
-      return this;
+	  this.stateTrigger.dispatchEvent(new Event("click"));
+	  return this;
   }
 
   update(value:DATA_TYPE|((value:DATA_TYPE, oldValue?:DATA_TYPE)=>DATA_TYPE), silent = false){
-      if(typeof value == "function"){
-         value = (value as ((value:DATA_TYPE, oldValue?:DATA_TYPE)=>DATA_TYPE))(this.value, this.oldValue);
-         this.oldValue = this.value;
-         this.value = value as DATA_TYPE;
-          if(!silent){
-              this.stateTrigger.dispatchEvent(new Event("click"));
-          }
-         return;
-      }
-      this.value = value;
-      if(!silent){
-          this.stateTrigger.dispatchEvent(new Event("click"));
-      }
+	  if(typeof value == "function"){
+		 value = (value as ((value:DATA_TYPE, oldValue?:DATA_TYPE)=>DATA_TYPE))(this.value, this.oldValue);
+		 this.oldValue = this.value;
+		 this.value = value as DATA_TYPE;
+		  if(!silent){
+			  this.stateTrigger.dispatchEvent(new Event("click"));
+		  }
+		 return;
+	  }
+	  this.value = value;
+	  if(!silent){
+		  this.stateTrigger.dispatchEvent(new Event("click"));
+	  }
   }
 
   subscribe(callback:(value:DATA_TYPE, oldValue?:DATA_TYPE)=>void){
-      const THIS = this;
-      this.stateTrigger.addEventListener("click", ()=>{
-          try{
-              callback(THIS.value, THIS.oldValue);
-          }catch(e){
-              console.log("Something happened in the subscription:");
-              console.log("FROM: ", callback);
-              console.log("VALUE:", THIS.value);
-              console.log("ERROR: ", e);
-              console.log("If this is intended (i.e accessing values from parallel subscription), ignore this message");
-          }
-      })
+	  const THIS = this;
+	  this.stateTrigger.addEventListener("click", ()=>{
+		  try{
+			  callback(THIS.value, THIS.oldValue);
+		  }catch(e){
+			  console.log("Something happened in the subscription:");
+			  console.log("FROM: ", callback);
+			  console.log("VALUE:", THIS.value);
+			  console.log("ERROR: ", e);
+			  console.log("If this is intended (i.e accessing values from parallel subscription), ignore this message");
+		  }
+	  })
   }
 }
